02129f119745e190c6ef0eb04f84b71e3b0e77af
Xenônio <102549312+Xonhonho@users.noreply.github.com>
2022-10-29 15:39:33 -0300

    Update 1_botao_on_off.c

--- 1_botao_on_off.c
+++ 1_botao_on_off.c
@@ -1,20 +1,14 @@
-#define BT	PIND.B2
-#define LED	PORTD.B5
+#define BT PIND.B2
+#define LED PORTD.B5
 
-#define off	0
-#define on	1
-
-void main (void)
-{
+void main (void){
 DDRD.B2 = 0;
 DDRD.B5 = 1;
-
-LED = off;
-
- while (1)
- {
-	while (BT == off);
-	while (BT == on);
+LED = 0;
+while (1){
+	while (BT == 0);
+	while (BT == 1){
 	LED = !LED;
- }
+	}
+}
 }
