c9c901c5df85c03f52b11c237453e229709b35c1
Xenônio <102549312+Xonhonho@users.noreply.github.com>
2022-10-31 17:57:01 -0300

    Update 11_potenciometro_dutycicle.c

--- 11_potenciometro_dutycicle.c
+++ 11_potenciometro_dutycicle.c
@@ -1,169 +1,123 @@
-#define Time_PID   PORTD.B5
+#define Time_PID PORTD.B5
+#define Data7 PORTB.B0
+#define Data6 PORTB.B1
+#define Data5 PORTB.B2
+#define Data4 PORTB.B3
+#define RS PORTB.B4
+#define EN PORTB.B5
+#define NumCol 16
+#define Linha1 0x80
+#define Linha2 0xC0
+#define Linha3 0x90
+#define Linha4 0xD0
+#define MSG1 0
+#define MSG2 1
 
-#define  Data7     PORTB.B0
-#define  Data6     PORTB.B1
-#define  Data5     PORTB.B2
-#define  Data4     PORTB.B3
-
-#define  RS        PORTB.B4
-#define  EN        PORTB.B5
-
-#define  NumCol    16       //Número de colunas do LCD utilizado (16x2)
-#define  Linha1    0x80     //1º endereço linha 1 do LCD
-#define  Linha2    0xC0     //1º endereço linha 2 do LCD
-#define  Linha3    0x90     //1º endereço linha 3 do LCD
-#define  Linha4    0xD0     //1º endereço linha 4 do LCD
-#define  MSG1      0        //Código da 1ª Mensagem
-#define  MSG2      1        //Código da 2ª Mensagem
-#define  MSG3      2        //Código da 3ª Mensagem
-#define  MSG4      3        //Código da 4ª Mensagem
-#define  MSG5      4        //Código da 5ª Mensagem
-#define  MSG6      5        //Código da 6ª Mensagem
-#define  MSG7      6        //Código da 7ª Mensagem
-#define  MSG8      7        //Código da 8ª Mensagem
-#define  MSG9      8        //Código da 9ª Mensagem
-#define  MSG10     9        //Código da 10ª Mensagem
-
-#define  Grau_C    0x00     //Código Grau Célsius
-#define  Seta_E    0x01     //Código Seta para Esquerda
-#define  Seta_D    0x02     //Código Seta para Direita
-#define  Seta_C    0x03     //Código Seta para Cima
-#define  Seta_B    0x04     //Código Seta para Baixo
-#define  Grau_F    0x05     //Código Grau Fahrenheit
-
-unsigned char Tab_Disp[10] = {0x03, 0x9F, 0x25, 0x0D, 0x99,
-                              0x49, 0x41, 0x1F, 0x01, 0x09};
-
-unsigned char Uni, Dez, Cen, Dec, display, Digitos[11], digito, cnt;
-
-double aux1, aux2, analog, measure, ideal_value, valorF, aux1F, aux2F, DecF, CenF, DezF, UniF;
-
+unsigned char Tab_Disp[10] = {0x03, 0x9F, 0x25, 0x0D, 0x99, 0x49, 0x41, 0x1F, 0x01, 0x09},
+Tab_CGRam[40] = {0x18, 0x18, 0x06, 0x09, 0x08, 0x09, 0x06, 0x00, 0x01, 0x03, 0x07, 0x0F, 0x07, 0x03, 0x01, 0x00,
+0x08, 0x0C, 0x0E, 0x0F, 0x0E, 0x0C, 0x08, 0x00, 0x00, 0x00, 0x04, 0x0E, 0x1F, 0x00, 0x00, 0x00, 0x00, 0x00, 0x1F, 0x0E, 0x04, 0x00, 0x00, 0x00},
+Tab_Conf[4] = {0x28, 0x06, 0x01, 0x0C},
+Tab_4Bit[4] = {0x30, 0x30, 0x30, 0x20},
+Uni, Dez, Cen, Dec, display, Digitos[11], digito, cnt;
+double aux1, aux2, analog;
 void pid_control();
 
-unsigned char
-Tab_CGRam[40] = {0x18, 0x18, 0x06, 0x09, 0x08, 0x09, 0x06, 0x00,
-                 0x01, 0x03, 0x07, 0x0F, 0x07, 0x03, 0x01, 0x00,
-                 0x08, 0x0C, 0x0E, 0x0F, 0x0E, 0x0C, 0x08, 0x00,
-                 0x00, 0x00, 0x04, 0x0E, 0x1F, 0x00, 0x00, 0x00,
-                 0x00, 0x00, 0x1F, 0x0E, 0x04, 0x00, 0x00, 0x00};
-unsigned char Tab_Conf[4] = {0x28,
-                             0x06,
-                             0x01,
-                             0x0C};
-
-unsigned char Tab_4Bit[4] = {0x30,
-                             0x30,
-                             0x30,
-                             0x20};
-
-void Disp_4bits (void)
-{
+void Disp_4bits(void){
  unsigned char y;
  display = 0xF0;
  delay_ms (100);
-
- for (y=0; y<=3; y++)
-    {
-      display = Tab_4Bit[y];
-      Data7 = display.B7;
-      Data6 = display.B6;
-      Data5 = display.B5;
-      Data4 = display.B4;
-      delay_ms (2);
-      RS = 0;
-      EN = 1;
-      delay_ms (2);
-      EN = 0;
-    }
-
-    for (y=0; y<=3; y++)
-    {
-      display = (Tab_Conf[y] & 0xF0);
-      Data7 = display.B7;
-      Data6 = display.B6;
-      Data5 = display.B5;
-      Data4 = display.B4;
-      delay_ms (2);
-      RS = 0;
-      EN = 1;
-      delay_ms (2);
-      EN = 0;
-
-      display = ((Tab_Conf[y]) << 4);
-      Data7 = display.B7;
-      Data6 = display.B6;
-      Data5 = display.B5;
-      Data4 = display.B4;
-      delay_ms (2);
-      RS = 0;
-      EN = 1;
-      delay_ms (2);
-      EN = 0;
-    }
+ for(y = 0; y <= 3; y++){
+  display = Tab_4Bit[y];
+  Data7 = display.B7;
+  Data6 = display.B6;
+  Data5 = display.B5;
+  Data4 = display.B4;
+  delay_ms(2);
+  RS = 0;
+  EN = 1;
+  delay_ms(2);
+  EN = 0;
+ }
+ for (y=0; y<=3; y++){
+  display = (Tab_Conf[y] & 0xF0);
+  Data7 = display.B7;
+  Data6 = display.B6;
+  Data5 = display.B5;
+  Data4 = display.B4;
+  delay_ms(2);
+  RS = 0;
+  EN = 1;
+  delay_ms(2);
+  EN = 0;
+  display = ((Tab_Conf[y]) << 4);
+  Data7 = display.B7;
+  Data6 = display.B6;
+  Data5 = display.B5;
+  Data4 = display.B4;
+  delay_ms(2);
+  RS = 0;
+  EN = 1;
+  delay_ms(2);
+  EN = 0;
+ }
 }
 
-void Load_GraphChar (unsigned char codigo)
-{
+void Load_GraphChar(unsigned char codigo){
  unsigned char z;
  display = ((0x40 + (codigo * 8)) & 0xF0);
  Data7 = display.B7;
  Data6 = display.B6;
  Data5 = display.B5;
  Data4 = display.B4;
- delay_ms (2);
+ delay_ms(2);
  RS = 0;
  EN = 1;
- delay_ms (2);
+ delay_ms(2);
  EN = 0;
-
  display = ((0x40 + (codigo * 8)) << 4);
  Data7 = display.B7;
  Data6 = display.B6;
  Data5 = display.B5;
  Data4 = display.B4;
- delay_ms (2);
+ delay_ms(2);
  RS = 0;
  EN = 1;
- delay_ms (2);
+ delay_ms(2);
  EN = 0;
-
- for (z=(codigo * 8); z<=(7 + (codigo * 8)); z++)
-  {
-   display = (Tab_CGRam[z] & 0xF0);
-   Data7 = display.B7;
-   Data6 = display.B6;
-   Data5 = display.B5;
-   Data4 = display.B4;
-   delay_ms (2);
-   RS = 1;
-   EN = 1;
-   delay_ms (2);
-   EN = 0;
-
-   display = ((Tab_CGRam[z]) << 4);
-   Data7 = display.B7;
-   Data6 = display.B6;
-   Data5 = display.B5;
-   Data4 = display.B4;
-   delay_ms (2);
-   RS = 1;
-   EN = 1;
-   delay_ms (2);
-   EN = 0;
-  }
+ for (z=(codigo * 8); z<=(7 + (codigo * 8)); z++){
+  display = (Tab_CGRam[z] & 0xF0);
+  Data7 = display.B7;
+  Data6 = display.B6;
+  Data5 = display.B5;
+  Data4 = display.B4;
+  delay_ms(2);
+  RS = 1;
+  EN = 1;
+  delay_ms(2);
+  EN = 0;
+  display = ((Tab_CGRam[z]) << 4);
+  Data7 = display.B7;
+  Data6 = display.B6;
+  Data5 = display.B5;
+  Data4 = display.B4;
+  delay_ms(2);
+  RS = 1;
+  EN = 1;
+  delay_ms(2);
+  EN = 0;
+ }
 }
 
-void Posi_Char (unsigned char Position)
-{
+void Posi_Char(unsigned char Position){
  display = (Position & 0xF0);
  Data7 = display.B7;
  Data6 = display.B6;
  Data5 = display.B5;
  Data4 = display.B4;
- delay_ms (2);
+ delay_ms(2);
  RS = 0;
  EN = 1;
- delay_ms (2);
+ delay_ms(2);
  EN = 0;
 
  display = ((Position) << 4);
@@ -171,195 +125,145 @@ void Posi_Char (unsigned char Position)
  Data6 = display.B6;
  Data5 = display.B5;
  Data4 = display.B4;
- delay_ms (2);
+ delay_ms(2);
  RS = 0;
  EN = 1;
- delay_ms (2);
+ delay_ms(2);
  EN = 0;
 }
 
-void Escreve_Char (unsigned char Num_CGRAM)
-{
-  display = (Num_CGRAM & 0xF0);
-  Data7 = display.B7;
-  Data6 = display.B6;
-  Data5 = display.B5;
-  Data4 = display.B4;
-  delay_ms (2);
-  RS = 1;
-  EN = 1;
-  delay_ms (2);
-  EN = 0;
+void Escreve_Char(unsigned char Num_CGRAM){
+ display = (Num_CGRAM & 0xF0);
+ Data7 = display.B7;
+ Data6 = display.B6;
+ Data5 = display.B5;
+ Data4 = display.B4;
+ delay_ms(2);
+ RS = 1;
+ EN = 1;
+ delay_ms(2);
+ EN = 0;
 
-  display = ((Num_CGRAM) << 4);
-  Data7 = display.B7;
-  Data6 = display.B6;
-  Data5 = display.B5;
-  Data4 = display.B4;
-  delay_ms (2);
-  RS = 1;
-  EN = 1;
-  delay_ms (2);
-  EN = 0;
+ display = ((Num_CGRAM) << 4);
+ Data7 = display.B7;
+ Data6 = display.B6;
+ Data5 = display.B5;
+ Data4 = display.B4;
+ delay_ms(2);
+ RS = 1;
+ EN = 1;
+ delay_ms(2);
+ EN = 0;
 }
 
-void Escreve_Frase (unsigned char Local)
-{
- unsigned char i;
-//                                      0123456789ABCDEF
- code unsigned char Message[10][17] = {"                ",
-                                       "                ",
-                                       "                ",
-                                       "                ",
-                                       " Brilho do LED: ",
-                                       "          %     ",
-                                       "                ",
-                                       "                ",
-                                       "                ",
-                                       "                "};
- for (i=0; i<=(NumCol-1); i++)
-  {
-   display = ((Message[Local][i]) & 0xF0);
-   Data7 = display.B7;
-   Data6 = display.B6;
-   Data5 = display.B5;
-   Data4 = display.B4;
-   delay_ms (2);
-   RS = 1;
-   EN = 1;
-   delay_ms (2);
-   EN = 0;
-
-   display = ((Message[Local][i]) << 4);
-   Data7 = display.B7;
-   Data6 = display.B6;
-   Data5 = display.B5;
-   Data4 = display.B4;
-   delay_ms (2);
-   RS = 1;
-   EN = 1;
-   delay_ms (2);
-   EN = 0;
+void Escreve_Frase(unsigned char Local){
+  unsigned char i;
+   code unsigned char Message[2][17] = {" Brilho do LED: ", "          %     "};
+  for(i = 0; i <= (NumCol-1); i++){
+    display = ((Message[Local][i]) & 0xF0);
+    Data7 = display.B7;
+    Data6 = display.B6;
+    Data5 = display.B5;
+    Data4 = display.B4;
+    delay_ms(2);
+    RS = 1;
+    EN = 1;
+    delay_ms(2);
+    EN = 0;
+    display = ((Message[Local][i]) << 4);
+    Data7 = display.B7;
+    Data6 = display.B6;
+    Data5 = display.B5;
+    Data4 = display.B4;
+    delay_ms(2);
+    RS = 1;
+    EN = 1;
+    delay_ms(2);
+    EN = 0;
   }
 }
-
-void Blink (void)
-{
- Posi_Char (0x0F);
+void Blink(void){
+  Posi_Char (0x0F);
 }
 
-void Cursor_Off (void)
-{
- Posi_Char (0x0C);
+void Cursor_Off(void){
+  Posi_Char (0x0C);
 }
 
-void Config_Ports (void)
-{
- DDRD.B6 = 1;
-
- DDRD.B5 = 1;
-
- DDRB.B0 = 1;
- DDRB.B1 = 1;
- DDRB.B2 = 1;
- DDRB.B3 = 1;
-
- DDRB.B4 = 1;
- DDRB.B5 = 1;
-
- ADCSRA  = 0x93;
+void Config_Ports(void){
+  DDRD.B6 = 1;
+  DDRD.B5 = 1;
+  DDRB.B0 = 1;
+  DDRB.B1 = 1;
+  DDRB.B2 = 1;
+  DDRB.B3 = 1;
+  DDRB.B4 = 1;
+  DDRB.B5 = 1;
+  PORTD.B6 = 0;
+  ADCSRA  = 0x93;
 }
 
-int AD_Conv (unsigned char canalAD)
-{
-   static int analogH, analogL;
-
-   ADMUX   = 0x40 | canalAD;
-
-   ADCSRA |= (1<<ADSC);
-
-   while(!(ADCSRA&=~(1<<ADIF)));
-
-   ADCSRA |= (1<<ADIF);
-
-   analogL = ADCL;
-   analogH = ADCH;
-
-   analog  = (analogH<<8) | analogL;
-
-   return analog;
-
-} //end ad_conv
-
-void converte (double valor)
-{
- aux1 = valor/100;
- aux2 = valor - 100*floor(valor/100);
-
- Dec = 10*(valor - floor(valor));
- Uni = aux2 - 10*floor(aux2/10);
- Dez = aux2/10;
- Cen = aux1 - 10*floor(aux1/10);
-
- valorF = 9*valor/5 + 32;
-
- aux1F = valorF/100;
- aux2F = valorF - 100*floor(valorF/100);
-
- DecF = 10*(valorF - floor(valorF));
- UniF = aux2F - 10*floor(aux2F/10);
- DezF = aux2F/10;
- CenF = aux1F - 10*floor(aux1F/10);
+int AD_Conv(unsigned char canalAD){
+  static int analogH, analogL;
+  ADMUX = 0x40 | canalAD;
+  ADCSRA |= (1<<ADSC);
+  while(!(ADCSRA&=~(1<<ADIF)));
+  ADCSRA |= (1<<ADIF);
+  analogL = ADCL;
+  analogH = ADCH;
+  analog  = (analogH<<8) | analogL;
+  return analog;
 }
 
-void mostra2 (void)
-{
- Posi_Char(Linha2 + 5);
+void converte(double x){
+  aux1 = x/100;
+  aux2 = x - 100*floor(aux1);
+
+  Dec = 10*(x - floor(x));
+  Uni = aux2 - (10 * floor(aux2 / 10));
+  Dez = aux2 / 10;
+  Cen = aux1 - (10 * floor(aux1 / 10));
+}
+
+void mostra(int n){
+ Posi_Char(Linha2 + n);
  Escreve_Char(Cen + '0');
- Posi_Char(Linha2 + 6);
+ Posi_Char(Linha2 + n + 1);
  Escreve_Char(Dez + '0');
- Posi_Char(Linha2 + 7);
+ Posi_Char(Linha2 + n + 2);
  Escreve_Char(Uni + '0');
- Posi_Char(Linha2 + 8);
+ Posi_Char(Linha2 + n + 3);
  Escreve_Char(',');
- Posi_Char(Linha2 + 9);
+ Posi_Char(Linha2 + n + 4);
  Escreve_Char(Dec + '0');
 }
 
-void Init_Timer0 (void)
-{
-    WGM00_Bit = 1;
-    WGM01_Bit = 1;
-    WGM02_Bit = 0;
-    COM0A1_Bit = 1;
-    COM0A0_Bit = 0;
-    TCCR0B.CS00 = 1;
-    TCCR0B.CS01 = 1;
-    TCCR0B.CS02 = 0;
-    OCR0A = 0;
+void Init_Timer0(void){
+  WGM00_Bit = 1;
+  WGM01_Bit = 1;
+  WGM02_Bit = 0;
+  COM0A1_Bit = 1;
+  COM0A0_Bit = 0;
+  TCCR0B.CS00 = 1;
+  TCCR0B.CS01 = 1;
+  TCCR0B.CS02 = 0;
+  OCR0A = 0;
 }
 
-void main (void)
-{
- Config_Ports ( );
- Disp_4bits ( );
- 
- DDRD.B6 = 1;
- PORTD.B6 = 0;
- 
- Init_Timer0();
-
- Posi_Char (Linha1);
- Escreve_Frase (MSG5);
- Posi_Char (Linha2);
- Escreve_Frase (MSG6);
-
- while (1)
- {
-  AD_Conv(4);
-  measure = analog;
-  converte(100*measure/1022);
-  mostra2( );
-  OCR0A = 254*analog/1022;
- }
+void main(void){
+  Config_Ports();
+  Disp_4bits();
+  
+  Init_Timer0();
+  
+  Posi_Char(Linha1);
+  Escreve_Frase(MSG1);
+  Posi_Char(Linha2);
+  Escreve_Frase(MSG2);
+  while(1){
+    AD_Conv(4);
+    converte(100*analog/1022);
+    mostra(5);
+    OCR0A = 254*analog/1022;
+  }
 }
